87e20b7b1a7f892835d40a69de57fe83d8fcda2f
Arkadiy <capark74@mail.ru>
2023-03-02 20:53:04 +0300

    Показаны собственники квартиры

--- property/admin.py
+++ property/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import Flat, Complaint, Owner
 
+
+class FlatInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ('owner', 'flat')
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address')
     readonly_fields = ('created_at',)
@@ -9,6 +15,10 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ('liked_by',)
+    inlines = [
+        FlatInline
+    ]
+
 
 
 class ComplaintAdmin(admin.ModelAdmin):
